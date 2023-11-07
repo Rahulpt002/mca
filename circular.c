18c7e3729806c71a1ee08adfc5b38afdc7288994
Rahulpt002 <146359399+Rahulpt002@users.noreply.github.com>
2023-11-07 11:09:11 +0530

    Add files via upload

--- circular.c
+++ circular.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
+#include<stdlib.h>
 #define SIZE 5
 int queue[SIZE];
 int front= -1,rear= -1;
 void enqueue(int value)
 {
-if((front==0&&rear==SIZE-1)||(rear==(front-1)%(SIZE-1)))
+if((front==0 && rear==SIZE-1) || (rear==(front-1)%(SIZE-1)))
 {
 printf("queue is full\n");
 }
@@ -13,110 +14,166 @@ else if(front==-1)
 front=rear=0;
 queue[rear]=value;
 }
-else
-{
-rear++;
-queue[rear]=value;
+    else
+    {
+        rear++;
+        queue[rear]=value;
+    }
 }
-}
+
 void dequeue()
 {
-if(front==-1)
-{
-print("queue is empty");
+    if(front==-1)
+    {
+        printf("queue is empty");
+    }
+    else if(front==rear)
+    {
+        printf("deleted element :%d\n",queue[front]);
+        front=rear=-1;
+    }
+    else if(front==SIZE-1)
+    {
+        printf("deleted element :%d\n",queue[front]);
+        front=0;
+    }
+    else
+    {
+        printf("deleted element :%d\n",queue[front]);
+        front++;
+    }
 }
-else if(front==rear)
-{
-printf("deleted element :%\n",queue[front]);
-front=rear=-1;
-}
-else if(front=SIZE-1)
-{
-printf("deleted element :%\n",queue[front]);
-front=0;
-}
-else
-{
-printf("deleted element :%\n",queue[front]);
-front++;
-}
-}
+
 int search(int value)
 {
+    if(front==-1)
+    {
+        printf("queue is empty cannot search elements.\n");
+        return -1;
+    }
+    else
+    {
+        int i=front;
+        if(front<=rear)
+        {
+            while(i<=rear)
+            {
+                if(queue[i]==value)
+                {
+                    printf("entered %d found at index %d.\n", value,i);
+                    return i;
+                }
+                i++;
+            }
+        }
+        else
+        {
+            while(i<=SIZE-1)
+            {
+                if(queue[i]==value)
+                {
+                    printf("entered %d found at index %d.\n", value,i);
+                    return i;
+                }
+                i++;
+            }
+            i=0;
+            while(i<=rear)
+            {
+                if(queue[i]==value)
+                {
+                    printf("entered %d found at index %d.\n" ,value,i);
+                    return i;
+                }
+                i++;
+            }
+        }
+        printf("element %d not found in the queue.\n",value);
+        return -1;
+    }
+}
+void display()
+{
 if(front==-1)
 {
-printf("queue is empty cannot search elemants.\n");
-return-1;
+printf("queue is empty \n");
 }
 else
 {
 int i=front;
+printf("queue elemants :");
 if(front<=rear)
 {
 while(i<=rear)
 {
-if(queue[i]==value)
-{
-prnitf("enterd %d found at index %d.\n", value,i);
-return i;
-}
+printf("%d",queue[i]);
 i++;
+}
 }
 else
 {
 while(i<=SIZE-1)
 {
-if(queue[i]==value)
-{
-prnitf("enterd %d found at index %d.\n", value,i);
-return i;
-}
+printf("%d",queue[i]);
 i++;
 }
 i=0;
 while(i<=rear)
 {
-if(queue[i]==value)
-{
-prnitf("enterd %d found at index %d.\n" ,value,i);
-return i;
-}
+printf("%d",queue[i]);
 i++;
 }
 }
-printf("elemant %d found in the queue.\n",value);
-return -1;
+printf("\n");
 }
 }
+
+
 int main()
 {
-int choice,value;
-do
-{
-printf("1.enqueue.\n");
-printf("2.dequeu\n");
-//printf("3.scearch\n");
-printf("4.exit\n");
-printf("enter your choice:");
-scanf("%d",&choice);
-switch(choice)
-{
-case 1:
-printf("enter the values to be enterd:");
-scanf("%d",&value);
-enqueue(value);
-break;
-case 2:
-dequeue();
-break;
-case 3:
-printf("enter elemant you want to search:");
-scanf("%d",&value);
-search(value);
-break;
-case 4:
-exit(0);
-}
-}
-while(choice!=5);
+    int choice,value;
+   
+    do
+    {
+        printf("1.enqueue.\n");
+        printf("2.dequeue\n");
+        printf("3.search\n");
+        printf("4.exit\n");
+        printf("5.display\n");
+       
+        printf("enter your choice:");
+       
+        scanf("%d",&choice);
+       
+        switch(choice)
+        {
+            case 1:
+                printf("enter the values to be entered:");
+                scanf("%d",&value);
+                enqueue(value);
+                break;
+               
+            case 2:
+                dequeue();
+                break;
+               
+            case 3:
+                printf("enter element you want to search:");
+                scanf("%d",&value);
+                search(value);
+                break;
+               
+            case 4:
+                exit(0);
+            case 5:
+                disblay();
+               
+            default:
+                printf("Invalid choice! Please enter a valid option.\n");
+               
+         } 
+         
+     } while(choice!=5);
+     
+     return 0;
 }
